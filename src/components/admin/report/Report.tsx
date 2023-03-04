88f4634a7335637b1d0e519866165dfe0c23cc16
allan <allankipchumba76@gmail.com>
2023-03-04 23:23:17 +0300

    i nitiated report section

--- src/components/admin/report/Report.tsx
+++ src/components/admin/report/Report.tsx
@@ -2,5 +2,22 @@ import React from "react";
 import styles from "./report.module.scss";
 
 export const Report = () => {
-  return <div>Report</div>;
+  return (
+    <div className={styles.report}>
+      <div className={styles.header}>
+        <h1>monthly report</h1>
+        <div className={styles["transactions-duration"]}>
+          <form>
+            <input type="number" placeholder="month" required />
+            <input type="number" placeholder="year" required />
+            <button className={`btn }`} style={{ padding: "7px" }}>
+              Get data
+            </button>
+          </form>
+        </div>
+      </div>
+
+      <div></div>
+    </div>
+  );
 };
