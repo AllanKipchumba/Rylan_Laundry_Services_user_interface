ddbab4636f55db294854ecc17e6ad88ebd734600
allan <allankipchumba76@gmail.com>
2023-03-10 10:46:44 +0300

    connected login form to auth api

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,9 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./login.module.scss";
 import logo from "../../assets/logo.png";
+import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
+import axios from "axios";
+import { BeatLoader } from "react-spinners";
+
+interface IState {
+  username: string;
+  password: string;
+}
 
 export const Login = () => {
-  const handleLogin = () => {};
+  const [user, setUser] = useState<IState>({ username: "", password: "" });
+  const { username, password } = user;
+  const [showPassword, setShowPassword] = useState<boolean>(false);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [loginFail, setLoginFail] = useState<boolean>(false);
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setUser((prevData) => ({ ...prevData, [name]: value }));
+  };
+
+  const handleLogin = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setLoading(true);
+
+    try {
+      await axios({
+        method: "post",
+        url: "http://localhost:5000/auth/login",
+        data: { username, password },
+      }).then((res) => {
+        console.log(res.data);
+        setLoading(false);
+      });
+    } catch (error) {
+      setLoading(false);
+      setLoginFail(true);
+      console.log(error);
+    }
+  };
 
   return (
     <div className={styles["login-wrapper"]}>
@@ -15,13 +52,41 @@ export const Login = () => {
           <h1>Laundry Management System</h1>
         </div>
 
-        <form>
-          <input type="text" placeholder="Username" required />
+        {loginFail && (
+          <div className={styles["login-fail"]}>
+            <p>Unable to login! Check your credentials</p>
+          </div>
+        )}
 
-          <input type="password" placeholder="Password" required />
+        <form onSubmit={handleLogin}>
+          <input
+            type="text"
+            name="username"
+            value={username}
+            onChange={(e) => handleInputChange(e)}
+            placeholder="Username"
+            required
+          />
+          <div className={styles["input-wrapper"]}>
+            <input
+              type={showPassword ? "text" : "password"}
+              name="password"
+              value={password}
+              onChange={(e) => handleInputChange(e)}
+              placeholder="Password"
+              required
+            />
+            <span onClick={() => setShowPassword(!showPassword)}>
+              {showPassword ? <AiOutlineEye /> : <AiOutlineEyeInvisible />}
+            </span>
+          </div>
 
-          <button type="submit" onClick={handleLogin} className={styles.btn}>
-            Login
+          <button type="submit" className={styles.btn}>
+            {loading ? (
+              <BeatLoader loading={loading} color="#fff" margin={4} size={17} />
+            ) : (
+              `Login`
+            )}
           </button>
         </form>
       </div>
